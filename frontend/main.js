a8620ab7f25588761f099b0926bff228275fff82
Sam H <samuelhussain@live.com>
2023-11-15 17:57:00 -0500

    added comments to clarify

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,6 @@
-var outputContainer = document.getElementById("output");
-var createMode = true;
+let outputContainer = document.getElementById("output");
+let createMode = true;
+//sets switch to create mode when page loads
 document.getElementById("toggle-mode").checked = false; 
 document
   .getElementById("toggle-mode")
@@ -7,7 +8,7 @@ document
 
 function updateToggleLabel() {
   createMode = !this.checked;
-  var toggleLabel = document.getElementById("toggle-label");
+  let toggleLabel = document.getElementById("toggle-label");
   toggleLabel.innerText = createMode ? "Create" : "Search";
   console.log("Toggle mode set to " + (createMode ? "Create" : "Search"));
 }
@@ -16,14 +17,16 @@ updateToggleLabel();
 
 function submitRecipe() {
   let requestType = createMode ? "add" : "search";
-  var outputContainer = document.getElementById("output");
-  var recipeName = document.getElementById("recipeName").value;
-  var ingredient1 = document.getElementById("ingredient1").value.trim();
-  var ingredient2 = document.getElementById("ingredient2").value.trim();
-  var ingredient3 = document.getElementById("ingredient3").value.trim();
-  var ingredient4 = document.getElementById("ingredient4").value.trim();
-  var ingredient5 = document.getElementById("ingredient5").value.trim();
-  var ingredients = [];
+  let outputContainer = document.getElementById("output");
+  //gets values from input fields
+  let recipeName = document.getElementById("recipeName").value;
+  let ingredient1 = document.getElementById("ingredient1").value.trim();
+  let ingredient2 = document.getElementById("ingredient2").value.trim();
+  let ingredient3 = document.getElementById("ingredient3").value.trim();
+  let ingredient4 = document.getElementById("ingredient4").value.trim();
+  let ingredient5 = document.getElementById("ingredient5").value.trim();
+  let ingredients = [];
+  //adds ingredients to array if not empty
   if (ingredient1) ingredients.push(ingredient1);
   if (ingredient2) ingredients.push(ingredient2);
   if (ingredient3) ingredients.push(ingredient3);
@@ -31,7 +34,7 @@ function submitRecipe() {
   if (ingredient5) ingredients.push(ingredient5);
   //adds recipe to frontend if in create mode
   if (createMode) {
-    var newRecipe = document.createElement("div");
+    let newRecipe = document.createElement("div");
     newRecipe.className = "recipe-output";
     newRecipe.innerHTML = `<strong>${recipeName}:</strong> ${ingredients.join(
       ", "
@@ -40,7 +43,7 @@ function submitRecipe() {
   } else {
     alert("Searching for recipe!");
   }
-
+  //clears input fields
   document.getElementById("recipeName").value = "";
   document.getElementById("ingredient1").value = "";
   document.getElementById("ingredient2").value = "";
@@ -59,10 +62,10 @@ function submitRecipe() {
 }
 
 function displaySearchResults(results) {
-  var output = document.getElementById("output");
+  let output = document.getElementById("output");
   output.innerHTML = "<h2>Search Results:</h2>";
   results.forEach((result) => {
-    var recipeDiv = document.createElement("div");
+    let recipeDiv = document.createElement("div");
     recipeDiv.className = "recipe-output";
     recipeDiv.innerHTML = `<strong>${
       result.name
