84768ba46ca59742eaf132e4d03b0faa01d6f7ed
Sam H <samuelhussain@live.com>
2023-11-15 00:51:02 -0500

    backend goes crazy

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+#allows us to use cross origin resource sharing
+CORS(app)
 
 
 @app.route('/hello')
 def hello():
     print("Hello, World!")
     return 'Hello, World!'
+
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    # get data from fetch request body
+    data = request.get_json()
+    ingredients = data['ingredients']
+    name = data['name']
+    print("Recipe: " + name)
+    print("Ingredients: " + str(ingredients))
+    #push to database
+    return 'Data received!'
 
 
 if __name__ == '__main__':
